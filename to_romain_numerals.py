73ba4cda49e81adb980bfb2bb53c68ae6c63d7ee
Mariya Tokareva <157952546+MariyaTokarevaa@users.noreply.github.com>
2025-01-14 15:14:59 +0400

    Update to_romain_numerals.py

--- to_romain_numerals.py
+++ to_romain_numerals.py
@@ -1,25 +1,39 @@
 ''' напишите функцию,
 которая на вход принимает любое целое число и
 возвращает его в римском представлении (как строку).'''
-num_dict = {
-    1000: "M",
-    900: "CM",
-    500: "D",
-    400: "CD",
-    100: "C",
-    90: "XC",
-    50: "L",
-    40: "XL",
-    10: "X",
-    9: "IX",
-    5: "V",
-    4: "IV",
-    1: "I",
-}
+def roman_to_integer(s):
+    # Словарь для сопоставления римских цифр с их значениями
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    total = 0
+    prev_value = 0
+    
+    # Проходим по строке римской цифры с конца к началу
+    for char in reversed(s):
+        current_value = roman_dict[char]
+        
+        # Если текущее значение меньше предыдущего, вычитаем его
+        if current_value < prev_value:
+            total -= current_value
+        else:
+            total += current_value
+        
+        # Обновляем предыдущее значение
+        prev_value = current_value
+    
+    return total
 
-def convert(data):
-    result = ""
-    for i in num_dict.keys():
-        b, data = divmod(data, i)
-        result += num_dict[i] * b
-    return result
+# Пример использования
+roman_numeral = input()
+result = roman_to_integer(roman_numeral)
+print(result) 
+
+
